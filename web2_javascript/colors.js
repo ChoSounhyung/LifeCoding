08e155b60f87737a5859c5ca542bf9d48a0e93bf
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-10-01 21:56:59 +0900

    라이브러리와 프래임워크, jQuery 실습

--- web2_javascript/colors.js
+++ web2_javascript/colors.js
@@ -1,20 +1,25 @@
 var Links = {
     setColor:function(color) {
-        var i = 0;
-        var alist = document.querySelectorAll('a');
-        while(i < alist.length) {
-            alist[i].style.color=color;
-            i += 1;
-        }
+        // var i = 0;
+        // var alist = document.querySelectorAll('a');
+        // while(i < alist.length) {
+        //     alist[i].style.color=color;
+        //     i += 1;
+        // }
+
+        // $('a')-> 모든 a태그를 jQuery로 제어하겠다는 뜻
+        $('a').css('color', color); 
     }
 }
 
 var Body = {
     setColor:function(color) {
-        document.querySelector('body').style.color=color;  
+        //document.querySelector('body').style.color=color;  
+        $('body').css('color', color);
     },  //객체의 프로퍼티를 구분할때는 ,를 찍어야함
     setBackgroundColor:function(color) {
-        document.querySelector('body').style.backgroundColor=color;
+        //document.querySelector('body').style.backgroundColor=color;
+        $('body').css('backgroundColor', color);
     }
 }
 
