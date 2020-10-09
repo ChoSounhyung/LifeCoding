6b82f82d713d796bee37fbf3a70c4e226983d003
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-10-09 15:35:32 +0900

    댓글 컴포넌트 만들기

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Comment from'./Comment.js';
+
+const comments = [
+  {name: 'Inje Lee', content: 'My comment!'},
+  {name: '현빈', content: '이게 최선입니까? 확실해요?'},
+  {name: 'Tony Stark', content: 'I am Iron Man.'},
+]
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{padding : 16, backgroundColor: '#282c34'}}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -14,11 +21,19 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+      <div>
+        {comments.map((comment, index) => {
+          return (
+            <Comment
+              name={comment.name}
+              content={comment.content} />
+          )
+        })}
+      </div>
     </div>
   );
 }
