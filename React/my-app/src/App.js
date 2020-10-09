a892cefd308211af803cfe9ead3b471950deb9c7
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-10-09 17:08:06 +0900

    Component Lifecycle Unmount

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 import Comment from'./Comment.js';
 
 const commentsFromServer = [
-  {id: 1, name: 'Inje Lee', content: 'My comment!'},
-  {id: 2, name: '현빈', content: '이게 최선입니까? 확실해요?'},
-  {id: 3, name: 'Tony Stark', content: 'I am Iron Man.'},
-  {id: 4, name: 'Cho Sounhyung', content: '모든일에 최선을 다하자'},
+  
 ];
 
 var timer;
@@ -18,16 +15,20 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      comments: [],
+      comments: [
+        {id: 1, name: 'Inje Lee', content: 'My comment!'},
+        {id: 2, name: '현빈', content: '이게 최선입니까? 확실해요?'},
+        {id: 3, name: 'Tony Stark', content: 'I am Iron Man.'},
+        {id: 4, name: 'Cho Sounhyung', content: '모든일에 최선을 다하자'},
+      ],
     };
   }
 
   componentDidMount() {
     let comments = this.state.comments;
     timer = setInterval(() => {
-      if (comments.length < commentsFromServer.length) {
-        let index = comments.length;
-        comments.push(commentsFromServer[index]);
+      if (comments.length > 0) {
+        comments.pop();
         this.setState({   //state는 직접 수정하면 안됨(setState 이용)
           comments: comments
         });
