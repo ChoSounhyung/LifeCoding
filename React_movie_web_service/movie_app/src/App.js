c1e2e8c0babdfc33d527775282e0b81810d7bd46
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-10-11 15:55:03 +0900

    Dynamic Component Generation

--- React_movie_web_service/movie_app/src/App.js
+++ React_movie_web_service/movie_app/src/App.js
@@ -1,23 +1,45 @@
 import React from 'react';
 
-function Food({fav}) {    // props.fav == {fav}
-  return <h3>I like {fav}</h3>;
+function Food({name, picture}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img scr={picture} />
+    </div>
+  );
 }
 
-// function Food(props) {
-// return <h3>I like {props.fav}</h3>;
-// }
+const foodILike = [
+  {
+    name: "kimchi",
+    image: "https://m.gochoochongak.com/web/product/big/201810/1cf1eb74d9cbb972ee0fd10d7f25e49c.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image: "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image: "https://recipe1.ezmember.co.kr/cache/recipe/2015/06/01/0467fec40dc5df750f026e7a87b4b1261.jpg"
+  },
+  {
+    name: "Doncasu",
+    image: "https://th2.tmon.kr/thumbs/image/423/336/e17/1b1492318_700x700_95_FIT.jpg"
+  },
+  {
+    name: "Kimbap",
+    image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg"
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!!!</h1>
-      <Food fav = "kimchi" />    {/* Food component에 kimchi라는 value로 porp(속성) fav를 줌 */}
-      <Food fav = "ramen" />
-      <Food fav = "samgiopsal" />
-      <Food fav = "chukumi" />
+      {foodILike.map(dish => (    //dish is object
+      <Food name = {dish.name} picture = {dish.image} />
+      ))}
     </div>
-  )
+  );
 }
 
 export default App;
