5b4d67f465b4cf5b7e1a59dd6873713990b9f8c7
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-10-11 15:21:14 +0900

    Reusable Components with JSX + Props

--- React_movie_web_service/movie_app/src/App.js
+++ React_movie_web_service/movie_app/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({fav}) {    // props.fav == {fav}
+  return <h3>I like {fav}</h3>;
+}
+
+// function Food(props) {
+// return <h3>I like {props.fav}</h3>;
+// }
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!!</h1>
-      <Potato />
+      <Food fav = "kimchi" />    {/* Food component에 kimchi라는 value로 porp(속성) fav를 줌 */}
+      <Food fav = "ramen" />
+      <Food fav = "samgiopsal" />
+      <Food fav = "chukumi" />
     </div>
   )
 }
