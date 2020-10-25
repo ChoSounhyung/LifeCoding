8c67a9f3fc124e4546dda6a4cf1fdf60bbed7428
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-10-26 02:02:12 +0900

    UI와 로직 분리 ViewModel

--- Android_Room/room_exam/app/src/main/java/kr/hs/mirim/room_exam/MainActivity.java
+++ Android_Room/room_exam/app/src/main/java/kr/hs/mirim/room_exam/MainActivity.java
@@ -2,6 +2,8 @@ package kr.hs.mirim.room_exam;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.room.Room;
 
 import android.os.AsyncTask;
@@ -27,36 +29,19 @@ public class MainActivity extends AppCompatActivity {
         mTodoEditText = findViewById(R.id.todo_edit);
         mResultTextView = findViewById(R.id.result_text);
 
-        //db 생성성
-        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db")
-                .build();
+        MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
 
         //UI 갱신
-        db.todoDao().getAll().observe(this, todos -> {
+        viewModel.getAll().observe(this, todos -> {
             //데이터가 변경 될 때마다 수행
             mResultTextView.setText(todos.toString());
         });
 
-
         //버튼 클릭 시 DB에 insert
         findViewById(R.id.add_button).setOnClickListener(view -> {
-            new InsertAsyncTask(db.todoDao())
-                    .execute(new Todo(mTodoEditText.getText().toString()));
+            viewModel.insert(new Todo(mTodoEditText.getText().toString()));
         });
     }
 
-    //비동기처리
-    private static class InsertAsyncTask extends AsyncTask<Todo, Void, Void> {
-        private TodoDao mTodoDao;
 
-        public InsertAsyncTask(TodoDao mTodoDao) {
-            this.mTodoDao = mTodoDao;
-        }
-
-        @Override
-        protected Void doInBackground(Todo... todos) {
-            mTodoDao.insert(todos[0]);
-            return null;
-        }
-    }
 }
