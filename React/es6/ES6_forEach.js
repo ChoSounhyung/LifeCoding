0785d08118a9dcbae12b26462d5ef8ef91898f88
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2021-03-03 23:59:13 +0900

    forEach()

---
+++ React/es6/ES6_forEach.js
@@ -0,0 +1,41 @@
+// js
+function parse(qs) {
+  var queryString = qs.substr(0); // queryString = 'banana=10&apple=20&orange=30'
+  var chunks = qs.split("&");
+  var result = {};
+  for (var i = 0; i < chunks.length; i++) {
+    var parts = chunks[i].split("=");
+    var key = parts[0]; // banana
+    var value = parts[1]; // 10
+    result[key] = value; // {banana : '10'}
+  }
+  return result; //{banana: "10", apple: "20", orange: "30"}
+}
+
+// ES6
+function parse(qs) {
+  var queryString = qs.substr(0); // queryString = 'banana=10&apple=20&orange=30'
+  var chunks = qs.split("&");
+  var result = {};
+  for (var i = 0; i < chunks.length; i++) {
+    var parts = chunks[i].split("=");
+    var key = parts[0]; // banana
+    var value = Number.isNaN(Number(parts[1])) ? parts[1] : Number(parts[1]); // 10
+    result[key] = value; // {banana : '10'}
+  }
+  return result; //{banana: "10", apple: "20", orange: "30"}
+}
+
+// more simply
+function parse(qs) {
+  const queryString = qs.substr(0);
+  const chunks = queryString.split("&");
+  let result = {};
+  chunks.forEach((chunk) => {
+    const parts = chunk.split("=");
+    const key = parts[0];
+    const value = Number.isNaN(Number(parts[1])) ? parts[1] : Number(parts[1]);
+    result[key] = value;
+  });
+  return result;
+}
