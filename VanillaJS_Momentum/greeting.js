501a5fc7445b6787c6f796dd24258c178e9e80b6
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2021-03-06 18:31:07 +0900

    Saving the User Name part Two

--- VanillaJS_Momentum/greeting.js
+++ VanillaJS_Momentum/greeting.js
@@ -1,9 +1,26 @@
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
-  greeting = document.querySelector("js-greetings");
+  greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); // event의 기본 동작을 없애기 위해(엔터 누르면 input에 입력한 정보가 사라짐)
+  const currentValue = input.value;
+  //console.log(currentValue);
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
 
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
@@ -14,7 +31,7 @@ function paintGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    // she is not
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
